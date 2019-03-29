07f7fa6f30d5487e1cfc9eb78d69fbdf1bcf4818
gmencoff <george.mencoff@gmail.com>
2019-03-29 15:47:15 -0400

    Changed handheld device message so that Arduino did not have buffer errors

--- Test/Test_sketch/Test_sketch.ino
+++ Test/Test_sketch/Test_sketch.ino
@@ -1,203 +1,48 @@
-/*
- * rosserial Publisher Example
- * Prints "hello world!"
- */
-
-// Use the following line if you have a Leonardo or MKR1000 
-//#define USE_USBCON 
-
-#include <ros.h>
 #include <Wire.h>
-#include <LIDARLite.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
-#include <EEPROM.h>
+#include <ros.h>
+#include <sensor_msgs/Imu.h>
 #include <handheld_device/handheld_device_data.h>
 #include <std_msgs/String.h>
 
-//Set the delay rate between fresh samples
-#define BNO055_SAMPLERATE_DELAY_MS (100)
+#define BNO055_SAMPLERATE_DELAY_MS (10)
 
-//Initialize the sensors
-LIDARLite myLidarLite;
-Adafruit_BNO055 bno = Adafruit_BNO055(55);
-
-// store Lidar Reading and lidar reading count
-int LidarDistance;
-int LidarReadingCount=0;
-
-// store IMU quaternion and accel vector
-imu::Quaternion imuQuat;
-imu::Vector<3> imuAccel;
-
-//Store Switch Sensor
-int laserSwitch=2;
-
-//Create a ros node handle and publish to the desired topic
 ros::NodeHandle nh;
 handheld_device::handheld_device_data handheld_device_data;
 ros::Publisher Handheld_device("Handheld_device",&handheld_device_data);
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
-void displayCalStatus(void)
-{
-    /* Get the four calibration values (0..3) */
-    /* Any sensor data reporting 0 should be ignored, */
-    /* 3 means 'fully calibrated" */
-    uint8_t system, gyro, accel, mag;
-    system = gyro = accel = mag = 0;
-    bno.getCalibration(&system, &gyro, &accel, &mag);
-
-    /* The data should be ignored until the system calibration is > 0 */
-    //Serial.print("\t");
-    if (!system)
-    {
-        //Serial.print("! ");
-    }
-
-    /* Display the individual values */
-    //Serial.print("Sys:");
-    //Serial.print(system, DEC);
-    //Serial.print(" G:");
-    //Serial.print(gyro, DEC);
-    //Serial.print(" A:");
-    //Serial.print(accel, DEC);
-    //Serial.print(" M:");
-    //Serial.print(mag, DEC);
-}
-
-void setup()
-{
-  Serial.begin(57600);
-  
-  nh.initNode();
+void setup(void)
+{ 
+  nh.initNode(); 
   nh.advertise(Handheld_device);
-  
-  myLidarLite.begin(0,false);
-  
+  Serial.begin(57600);
+  //Serial.println("Orientation Sensor Test");
+  /* Initialise the sensor */
   if(!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
-    //Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    Serial.print(F("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!"));
     while(1);
   }
-  
-  int eeAddress = 0;
-  long bnoID;
-  bool foundCalib = false;
-
-  EEPROM.get(eeAddress, bnoID);
-
-  adafruit_bno055_offsets_t calibrationData;
-  sensor_t sensor;
-  
-  bno.getSensor(&sensor);
-  if (bnoID != sensor.sensor_id)
-  {
-      //Serial.println("\nNo Calibration Data for this sensor exists in EEPROM");
-      delay(500);
-  }
-  else
-  {
-      //Serial.println("\nFound Calibration for this sensor in EEPROM.");
-      eeAddress += sizeof(long);
-      EEPROM.get(eeAddress, calibrationData);
-
-      //Serial.println("\n\nRestoring Calibration data to the BNO055...");
-      bno.setSensorOffsets(calibrationData);
-
-      //Serial.println("\n\nCalibration data loaded into BNO055");
-      foundCalib = true;
-  }
-  
-  //Crystal must be configured AFTER loading calibration data into BNO055.
-  bno.setExtCrystalUse(true);
-  
-  sensors_event_t event;
-  bno.getEvent(&event);
-  if (foundCalib){
-      //Serial.println("Move sensor slightly to calibrate magnetometers");
-      while (!bno.isFullyCalibrated())
-      {
-          bno.getEvent(&event);
-          delay(BNO055_SAMPLERATE_DELAY_MS);
-      }
-  }
-  else
-  {
-      //Serial.println("Please Calibrate Sensor: ");
-      while (!bno.isFullyCalibrated())
-      {
-          bno.getEvent(&event);
-
-          //Serial.print("X: ");
-          //Serial.print(event.orientation.x, 4);
-          //Serial.print("\tY: ");
-          //Serial.print(event.orientation.y, 4);
-          //Serial.print("\tZ: ");
-          //Serial.print(event.orientation.z, 4);
-
-          /* Optional: Display calibration status */
-          displayCalStatus();
-
-          /* New line for the next sample */
-          //Serial.println("");
-
-          /* Wait the specified delay before requesting new data */
-          delay(BNO055_SAMPLERATE_DELAY_MS);
-      }
-  }
-  
-  adafruit_bno055_offsets_t newCalib;
-  bno.getSensorOffsets(newCalib);
-
-  //Serial.println("\n\nStoring calibration data to EEPROM...");
-
-  eeAddress = 0;
-  bno.getSensor(&sensor);
-  bnoID = sensor.sensor_id;
-
-  EEPROM.put(eeAddress, bnoID);
-
-  eeAddress += sizeof(long);
-  EEPROM.put(eeAddress, newCalib);
-  //Serial.println("Data stored to EEPROM.");
-
-  //Serial.println("\n--------------------------------\n");
-  
-    //set switch pin to input
-  pinMode(laserSwitch, INPUT);
-  
-
-  
   delay(500);
-  
-  
 }
-
-void loop()
+void loop(void)
 {
-  if(LidarReadingCount%100==0){
-    LidarDistance=myLidarLite.distance(); //read with reciever bias correction
-    LidarReadingCount=1;
-  }
-  else{
-    LidarDistance=myLidarLite.distance(false); //read without reciever bias correction
-    LidarReadingCount=LidarReadingCount+1;
-  }
-  
-  delay(BNO055_SAMPLERATE_DELAY_MS);
-  
-  // read the IMU quaternion and acceleration information, with gravity removed
-  //imuQuat=bno.getQuat();
-  //imuAccel=bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-  
-  //delay(BNO055_SAMPLERATE_DELAY_MS);
-  
-  //read switch state
-  //int switchState=digitalRead(laserSwitch);
+  imu::Quaternion quat = bno.getQuat();
+  imu::Vector<3> linaccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+  handheld_device_data.device_quat.x = quat.x();
+  handheld_device_data.device_quat.y = quat.y();
+  handheld_device_data.device_quat.z = quat.z();
+  handheld_device_data.device_quat.w = quat.w();
+  handheld_device_data.device_accel.linear.x = linaccel(0);
+  handheld_device_data.device_accel.linear.y = linaccel(1);
+  handheld_device_data.device_accel.linear.z = linaccel(2);
   
   Handheld_device.publish(&handheld_device_data);
   nh.spinOnce();
-  delay(1000);
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+  //Serial.print("In Loop");
 }
--- libraries/ros_lib/handheld_device/handheld_device_data.h
+++ libraries/ros_lib/handheld_device/handheld_device_data.h
@@ -5,8 +5,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include "ros/msg.h"
-#include "geometry_msgs/Quaternion.h"
-#include "geometry_msgs/Accel.h"
 
 namespace handheld_device
 {
@@ -14,19 +12,34 @@ namespace handheld_device
   class handheld_device_data : public ros::Msg
   {
     public:
-      typedef int64_t _device_distance_type;
+      typedef int16_t _device_distance_type;
       _device_distance_type device_distance;
-      typedef geometry_msgs::Quaternion _device_quat_type;
-      _device_quat_type device_quat;
-      typedef geometry_msgs::Accel _device_accel_type;
-      _device_accel_type device_accel;
-      typedef int64_t _device_switch_type;
+      typedef float _quatx_type;
+      _quatx_type quatx;
+      typedef float _quaty_type;
+      _quaty_type quaty;
+      typedef float _quatz_type;
+      _quatz_type quatz;
+      typedef float _quatw_type;
+      _quatw_type quatw;
+      typedef float _accelx_type;
+      _accelx_type accelx;
+      typedef float _accely_type;
+      _accely_type accely;
+      typedef float _accelz_type;
+      _accelz_type accelz;
+      typedef int8_t _device_switch_type;
       _device_switch_type device_switch;
 
     handheld_device_data():
       device_distance(0),
-      device_quat(),
-      device_accel(),
+      quatx(0),
+      quaty(0),
+      quatz(0),
+      quatw(0),
+      accelx(0),
+      accely(0),
+      accelz(0),
       device_switch(0)
     {
     }
@@ -35,34 +48,89 @@ namespace handheld_device
     {
       int offset = 0;
       union {
-        int64_t real;
-        uint64_t base;
+        int16_t real;
+        uint16_t base;
       } u_device_distance;
       u_device_distance.real = this->device_distance;
       *(outbuffer + offset + 0) = (u_device_distance.base >> (8 * 0)) & 0xFF;
       *(outbuffer + offset + 1) = (u_device_distance.base >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (u_device_distance.base >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (u_device_distance.base >> (8 * 3)) & 0xFF;
-      *(outbuffer + offset + 4) = (u_device_distance.base >> (8 * 4)) & 0xFF;
-      *(outbuffer + offset + 5) = (u_device_distance.base >> (8 * 5)) & 0xFF;
-      *(outbuffer + offset + 6) = (u_device_distance.base >> (8 * 6)) & 0xFF;
-      *(outbuffer + offset + 7) = (u_device_distance.base >> (8 * 7)) & 0xFF;
       offset += sizeof(this->device_distance);
-      offset += this->device_quat.serialize(outbuffer + offset);
-      offset += this->device_accel.serialize(outbuffer + offset);
       union {
-        int64_t real;
-        uint64_t base;
+        float real;
+        uint32_t base;
+      } u_quatx;
+      u_quatx.real = this->quatx;
+      *(outbuffer + offset + 0) = (u_quatx.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_quatx.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_quatx.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_quatx.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->quatx);
+      union {
+        float real;
+        uint32_t base;
+      } u_quaty;
+      u_quaty.real = this->quaty;
+      *(outbuffer + offset + 0) = (u_quaty.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_quaty.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_quaty.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_quaty.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->quaty);
+      union {
+        float real;
+        uint32_t base;
+      } u_quatz;
+      u_quatz.real = this->quatz;
+      *(outbuffer + offset + 0) = (u_quatz.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_quatz.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_quatz.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_quatz.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->quatz);
+      union {
+        float real;
+        uint32_t base;
+      } u_quatw;
+      u_quatw.real = this->quatw;
+      *(outbuffer + offset + 0) = (u_quatw.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_quatw.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_quatw.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_quatw.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->quatw);
+      union {
+        float real;
+        uint32_t base;
+      } u_accelx;
+      u_accelx.real = this->accelx;
+      *(outbuffer + offset + 0) = (u_accelx.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_accelx.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_accelx.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_accelx.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->accelx);
+      union {
+        float real;
+        uint32_t base;
+      } u_accely;
+      u_accely.real = this->accely;
+      *(outbuffer + offset + 0) = (u_accely.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_accely.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_accely.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_accely.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->accely);
+      union {
+        float real;
+        uint32_t base;
+      } u_accelz;
+      u_accelz.real = this->accelz;
+      *(outbuffer + offset + 0) = (u_accelz.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_accelz.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_accelz.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_accelz.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->accelz);
+      union {
+        int8_t real;
+        uint8_t base;
       } u_device_switch;
       u_device_switch.real = this->device_switch;
       *(outbuffer + offset + 0) = (u_device_switch.base >> (8 * 0)) & 0xFF;
-      *(outbuffer + offset + 1) = (u_device_switch.base >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (u_device_switch.base >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (u_device_switch.base >> (8 * 3)) & 0xFF;
-      *(outbuffer + offset + 4) = (u_device_switch.base >> (8 * 4)) & 0xFF;
-      *(outbuffer + offset + 5) = (u_device_switch.base >> (8 * 5)) & 0xFF;
-      *(outbuffer + offset + 6) = (u_device_switch.base >> (8 * 6)) & 0xFF;
-      *(outbuffer + offset + 7) = (u_device_switch.base >> (8 * 7)) & 0xFF;
       offset += sizeof(this->device_switch);
       return offset;
     }
@@ -71,42 +139,104 @@ namespace handheld_device
     {
       int offset = 0;
       union {
-        int64_t real;
-        uint64_t base;
+        int16_t real;
+        uint16_t base;
       } u_device_distance;
       u_device_distance.base = 0;
-      u_device_distance.base |= ((uint64_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_device_distance.base |= ((uint64_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_device_distance.base |= ((uint64_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_device_distance.base |= ((uint64_t) (*(inbuffer + offset + 3))) << (8 * 3);
-      u_device_distance.base |= ((uint64_t) (*(inbuffer + offset + 4))) << (8 * 4);
-      u_device_distance.base |= ((uint64_t) (*(inbuffer + offset + 5))) << (8 * 5);
-      u_device_distance.base |= ((uint64_t) (*(inbuffer + offset + 6))) << (8 * 6);
-      u_device_distance.base |= ((uint64_t) (*(inbuffer + offset + 7))) << (8 * 7);
+      u_device_distance.base |= ((uint16_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_device_distance.base |= ((uint16_t) (*(inbuffer + offset + 1))) << (8 * 1);
       this->device_distance = u_device_distance.real;
       offset += sizeof(this->device_distance);
-      offset += this->device_quat.deserialize(inbuffer + offset);
-      offset += this->device_accel.deserialize(inbuffer + offset);
       union {
-        int64_t real;
-        uint64_t base;
+        float real;
+        uint32_t base;
+      } u_quatx;
+      u_quatx.base = 0;
+      u_quatx.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_quatx.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_quatx.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_quatx.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->quatx = u_quatx.real;
+      offset += sizeof(this->quatx);
+      union {
+        float real;
+        uint32_t base;
+      } u_quaty;
+      u_quaty.base = 0;
+      u_quaty.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_quaty.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_quaty.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_quaty.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->quaty = u_quaty.real;
+      offset += sizeof(this->quaty);
+      union {
+        float real;
+        uint32_t base;
+      } u_quatz;
+      u_quatz.base = 0;
+      u_quatz.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_quatz.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_quatz.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_quatz.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->quatz = u_quatz.real;
+      offset += sizeof(this->quatz);
+      union {
+        float real;
+        uint32_t base;
+      } u_quatw;
+      u_quatw.base = 0;
+      u_quatw.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_quatw.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_quatw.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_quatw.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->quatw = u_quatw.real;
+      offset += sizeof(this->quatw);
+      union {
+        float real;
+        uint32_t base;
+      } u_accelx;
+      u_accelx.base = 0;
+      u_accelx.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_accelx.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_accelx.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_accelx.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->accelx = u_accelx.real;
+      offset += sizeof(this->accelx);
+      union {
+        float real;
+        uint32_t base;
+      } u_accely;
+      u_accely.base = 0;
+      u_accely.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_accely.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_accely.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_accely.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->accely = u_accely.real;
+      offset += sizeof(this->accely);
+      union {
+        float real;
+        uint32_t base;
+      } u_accelz;
+      u_accelz.base = 0;
+      u_accelz.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_accelz.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_accelz.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_accelz.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->accelz = u_accelz.real;
+      offset += sizeof(this->accelz);
+      union {
+        int8_t real;
+        uint8_t base;
       } u_device_switch;
       u_device_switch.base = 0;
-      u_device_switch.base |= ((uint64_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_device_switch.base |= ((uint64_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_device_switch.base |= ((uint64_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_device_switch.base |= ((uint64_t) (*(inbuffer + offset + 3))) << (8 * 3);
-      u_device_switch.base |= ((uint64_t) (*(inbuffer + offset + 4))) << (8 * 4);
-      u_device_switch.base |= ((uint64_t) (*(inbuffer + offset + 5))) << (8 * 5);
-      u_device_switch.base |= ((uint64_t) (*(inbuffer + offset + 6))) << (8 * 6);
-      u_device_switch.base |= ((uint64_t) (*(inbuffer + offset + 7))) << (8 * 7);
+      u_device_switch.base |= ((uint8_t) (*(inbuffer + offset + 0))) << (8 * 0);
       this->device_switch = u_device_switch.real;
       offset += sizeof(this->device_switch);
      return offset;
     }
 
     const char * getType(){ return "handheld_device/handheld_device_data"; };
-    const char * getMD5(){ return "894e9b259d1d4e8448e9e2d9ffed9851"; };
+    const char * getMD5(){ return "5f3fc4e797e13ce34dcd40452711b663"; };
 
   };
 
